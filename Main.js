6da39e8c579e2d10a50520f8c012b612ed87d065
milvio123 <happyshah909@gmail.com>
2022-09-05 18:45:34 -0700

    getpopularshow

--- Main.js
+++ Main.js
@@ -2,9 +2,11 @@ let baseURL = 'https://api.themoviedb.org/3/';
         let configData = null;
         let baseImageURL = null;
         const key = "34bff23dc0e6e198d082c23f332be9b2"
+        var language = "en-US"
         var array = []
-        let getPopular = function() {
-            console.log(array)
+
+        let getPopularMovie = function() {
+        
         let getConfig = function () {
             let url = "".concat(baseURL, 'configuration?api_key=', key); 
             fetch(url)
@@ -19,25 +21,66 @@ let baseURL = 'https://api.themoviedb.org/3/';
                 runSearch()
             })
             .catch(function(err){
-                alert(err);
+
             });
         }
         
         let runSearch = function () {
-            let url = ''.concat(baseURL, 'movie/popular?api_key=', key);
+            language = document.getElementById("getLanguage").value
+            let url = ''.concat(baseURL, 'movie/popular?api_key=', key, '&language=', language);
             fetch(url)
             .then(result=>result.json())
-            .then((data)=>{
-                //process the returned data
-                
+            .then((data)=>{                
               var Results = data.results
               var i = 0
               while(i<Results.length) {
               var what = data.results[i].title
               array.push(what)
               i++
-              }                
+              }    
+              alert(array)            
             })
         }
         getConfig()
+
+    }
+
+    let getPopularShow = function() {
+
+        let getConfig = function () {
+            let url = "".concat(baseURL, 'configuration?api_key=', key); 
+            fetch(url)
+            .then((result)=>{
+                return result.json();
+            })
+            .then((data)=>{
+                baseImageURL = data.images.secure_base_url;
+                configData = data.images;
+                console.log('config:', data);
+                console.log('config fetched');
+                runSearch()
+            })
+            .catch(function(err){
+                alert(err)
+            });
+        }
+        
+        let runSearch = function () {
+            language = document.getElementById("getLanguage").value
+            let url = ''.concat(baseURL, 'tv/popular?api_key=', key, '&language=', language);
+            fetch(url)
+            .then(result=>result.json())
+            .then((data)=>{                
+              var Results = data.results
+              var i = 0
+              while(i<Results.length) {
+              var what = data.results[i].name
+              array.push(what)
+              i++
+              }    
+              alert(array)            
+            })
+        }
+        getConfig()
+
     }
